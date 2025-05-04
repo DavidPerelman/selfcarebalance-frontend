6e7e85707aa047319bac45439cff5f2ce5a1f655
DavidPerelman <53565536+DavidPerelman@users.noreply.github.com>
2025-05-04 23:35:01 +0300

    try to fix vercel deploy

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -1,10 +1,10 @@
-import GoogleCallbackContent from "@/components/GoogleCallbackContent";
+import CallbackContent from "@/components/CallbackContent";
 import { Suspense } from "react";
 
-export default function GoogleCallbackPage() {
+export default function CallbackPage() {
   return (
     <Suspense fallback={<p>מתחברים לחשבון גוגל...</p>}>
-      <GoogleCallbackContent />
+      <CallbackContent />
     </Suspense>
   );
 }
